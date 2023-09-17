8c2af52e8e13b0339df61fa3d12c5e550da56e01
Johnny Kamigashima <johnny@jhk.app>
2023-09-17 01:51:17 -0300

    normalizado o audio

--- library/merge_mp3_files.py
+++ library/merge_mp3_files.py
@@ -1,6 +1,7 @@
 """ Concatena mp3 files
 """
 from pydub import AudioSegment
+from pydub.utils import make_chunks
     # Exemplo de uso
     # mp3_files = ["file1.mp3", "file2.mp3", "file3.mp3"]
     # output_file = "merged.mp3"
@@ -23,3 +24,38 @@ def merge_mp3_files(mp3_files, output_file, audio_extension="mp3"):
         combined += audio
 
     combined.export(output_file, format=audio_extension, bitrate="320k")
+
+
+def normalize_audio(file_name):
+    """
+    Normalize the audio file by loading it, dividing it into 1-second segments,
+    calculating the RMS value of each segment, and normalizing the volume. 
+    Then, join the normalized segments and export the normalized audio to a new MP3 file.
+
+    Args:
+        file_name (str): The name of the MP3 file to be normalized.
+
+    Returns:
+        None
+    """
+    # Carrega o arquivo MP3
+    audio = AudioSegment.from_file(file_name, format="mp3")
+
+    # Divide o áudio em segmentos de 1 segundo
+    chunks = make_chunks(audio, 1000)
+
+    # Calcula o valor RMS de cada segmento e normaliza o volume
+    normalized_chunks = []
+    for chunk in chunks:
+        rms = chunk.rms
+        normalized_chunk = chunk - (rms - 100)
+        normalized_chunks.append(normalized_chunk)
+
+    # Junta os segmentos normalizados
+    normalized_audio = AudioSegment.from_mono_audiosegments(*normalized_chunks)
+
+    # Exporta o áudio normalizado para um novo arquivo MP3
+    normalized_file_name = file_name.replace(".mp3", "_normalized.mp3")
+    normalized_audio.export(normalized_file_name, format="mp3")
+
+    print("Áudio normalizado salvo como:", normalized_file_name)
