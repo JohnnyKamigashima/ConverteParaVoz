3bc2ad88a1d07efb156ce4a21a6af32033df192c
Johnny Kamigashima <johnny@jhk.app>
2024-08-12 23:08:42 -0300

    resuma

--- converte_para_voz.py
+++ converte_para_voz.py
@@ -37,7 +37,7 @@ for line in contents.split('\n'):
 MODEL = 'gpt-4o-mini'
 
 # Defining the bot's personality using adjectives
-BOT_PERSONALITY = 'Traduza o texto para português do Brasil, \
+BOT_PERSONALITY = 'Resuma o texto para português do Brasil, \
     corrigindo com pontuação correta para uma melhor leitura, \
         detalhando cada idéia mencionada no texto de forma clara e simples que qualquer pessoa leiga consiga entender sem inventar novas idéias nem criar novos sentidos. \
             Se houver trechos de códigos, descreva a funcionalidade do código e o resultado gerado por ele e remova os trechos de código.\
